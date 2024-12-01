ff810bfd93bd46a81a0c6217a706ab205a30cd32
higorvieira-dev <higorvieira.dev@hotmail.com>
2024-12-01 10:08:40 -0300

    modificado visuais do site

--- animation.js
+++ animation.js
@@ -1,3 +1,27 @@
 function myFunction(){
     document.getElementById("myDialog").showModal();
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const bars = document.querySelectorAll(".bar");
+  
+    bars.forEach(bar => {
+      const value = bar.getAttribute("data-value");
+      bar.style.height = `${value}%`;
+  
+      const valueDisplay = bar.querySelector(".value");
+      let current = 0;
+  
+      // Animação para exibir o valor gradualmente
+      const interval = setInterval(() => {
+        if (current >= value) {
+          clearInterval(interval);
+        } else {
+          current++;
+          valueDisplay.textContent = `${current}%`;
+        }
+      }, 10);
+    });
+  });
+  
